366bf2c22aa300965b6cf1a79e865c1ad1bcf53e
auto_serial_at <18674622237@163.com>
2024-05-24 21:22:21 +0800

    更新多指令发送显示功能

---
+++ functions/instruction_func.py
@@ -0,0 +1,55 @@
+from PyQt5.QtWidgets import QHBoxLayout, QLineEdit, QLabel
+from functions.tool import Tool
+from config.handel_config import sys_, instruction_config
+
+
+class CreateInstruction:
+    def __init__(self, ui):
+        super().__init__()
+        self.ui = ui
+        self.tool = Tool()
+
+    def create_instruction_closure(self, line_edit):
+        """实现点击"""
+
+        def button_clicked():
+            line_edit_text = line_edit.text()
+            self._update_command(line_edit_text)
+
+        return button_clicked
+
+    def _update_command(self, text):
+        """更新写入指令"""
+        if self.ui.command_line.text():
+            self.ui.command_line.clear()
+        self.ui.command_line.setText(text)
+        self.ui.send_btn.click()  # 点击按钮下发指令
+
+    def create_widget(self):
+        """添加指令"""
+        try:
+            self.commands = self.tool.read_csv_by_command(instruction_config)
+        except Exception:
+            path = sys_ + "\\" + "instruction_config.csv"
+            self.commands = self.tool.read_csv_by_command(path)  # 打包时用到路径
+
+        for command in self.commands:
+            print(command)
+            # 为每个命令创建一个水平布局
+            row_layout = QHBoxLayout()
+            line_edit = QLineEdit(command[0])
+            label_timer = QLabel(command[1])
+            label_timer.setFixedSize(100,30)
+            label_send = QLabel("待发送")
+            label_timer.setFixedSize(80, 30)
+
+            # 注意lambda函数中传递line_edit作为参数来避免late binding问题
+            # label.clicked.connect(lambda checked, le=line_edit: self.create_instruction_closure(le)())
+
+            # 将控件添加到水平布局
+            row_layout.addWidget(line_edit)
+            row_layout.addWidget(label_timer)
+            row_layout.addWidget(label_send)
+
+            # 将水平布局添加到已存在的 frame 的垂直布局中
+            self.ui.frame_2.layout().addLayout(row_layout)  # 使用 layout() 方法获取现有布局并添加内容
---
+++ functions/tool.py
@@ -0,0 +1,20 @@
+import csv
+
+
+class Tool:
+
+    def read_csv_by_command(self, file_path):
+        """
+        读取 CSV 文件，并按列返回数据。
+
+        :return: 一个字典，键为列名，值为对应列的数据列表
+        """
+        with open(file_path, newline='', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            return [row for row in reader]
+
+
+# 使用示例
+# if __name__ == '__main__':
+#     csv_reader = Tool()
+#     result = csv_reader.read_csv_by_command('../config/instruction_config.csv')
