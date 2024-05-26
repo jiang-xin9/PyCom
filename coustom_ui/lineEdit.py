164e927d6f45308124632f908acae70f1b5be58e
清安无别事 <18674622237@163.com>
2024-05-26 12:01:48 +0800

    修改line样式

--- coustom_ui/lineEdit.py
+++ coustom_ui/lineEdit.py
@@ -1,16 +1,15 @@
 from PyQt5.QtGui import QFontMetrics
 from PyQt5.QtWidgets import QLineEdit, QSizePolicy
 
+
 class NewLineEdit(QLineEdit):
     def __init__(self, text="", parent=None):
         super().__init__(text, parent)
         self.setMouseTracking(True)
         self.default_style = """
             QLineEdit {
-                border: 0.5px solid rgb(64, 158, 255);
-                background-color: rgb(255, 255, 255);
+                border: 1px solid #ced4da;
                 border-radius: 4px;
-                color: rgb(64, 158, 255);
                 padding: 5px 10px;
                 font-size: 10px;
                 font-family: 'Microsoft YaHei';
@@ -26,7 +25,7 @@ class NewLineEdit(QLineEdit):
         font_metrics = QFontMetrics(self.font())
         text_width = font_metrics.width(self.text()) + 20
         self.setMinimumWidth(text_width)  # 设置最小宽度而不是固定宽度
-
+        self.setCursorPosition(0)  # 设置光标位置为最左边
     def enterEvent(self, event):
         super().enterEvent(event)
 
