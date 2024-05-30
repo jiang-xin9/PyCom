a9903442d69b03fc1f33ad5356378da28107da9a
auto_serial_at <18674622237@163.com>
2024-05-30 16:13:28 +0800

    后续去除该文件

--- config/enum_config.py
+++ config/enum_config.py
@@ -5,7 +5,7 @@ class SidebarParameters(Enum):
     # back_and_expand_func
     EXPANDED = 1            # 展开
     COLLAPSED = 0           # 关闭
-    WIDTH_EXPANDED = 180    # frame/widget最大宽度
+    WIDTH_EXPANDED = 200    # frame/widget最大宽度
     WIDTH_COLLAPSED = 0     # frame/widget最小宽度
     ANIM_DURATION = 150  # 动画持续时间（毫秒）
     ANIM_STEP = 10  # 动画步长
