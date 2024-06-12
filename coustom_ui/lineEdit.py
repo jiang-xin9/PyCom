e77fa14902f6bb1f2d6512e68bd3365dd022dcfc
清安无别事 <18674622237@163.com>
2024-06-12 15:16:28 +0800

    修改line，兼容placeholdertext属性

--- coustom_ui/lineEdit.py
+++ coustom_ui/lineEdit.py
@@ -1,17 +1,21 @@
 from PyQt5.QtGui import QFontMetrics
-from PyQt5.QtWidgets import QLineEdit, QSizePolicy
+from PyQt5.QtWidgets import QLineEdit, QSizePolicy, QWidget
+
 
 class NewLineEdit(QLineEdit):
     def __init__(self, text="", parent=None):
+        # Handle different types of initializations
+        if isinstance(text, QWidget):
+            parent = text
+            text = ""
         super().__init__(text, parent)
+
         self.setMouseTracking(True)
         self.default_style = """
             QLineEdit {
                 border: 1px solid #ced4da;
                 border-radius: 4px;
                 padding: 5px 10px;
-                font-size: 10px;
-                font-family: 'Microsoft YaHei';
             }
         """
         self.setStyleSheet(self.default_style)
@@ -25,7 +29,7 @@ class NewLineEdit(QLineEdit):
 
     def adjustSizeToText(self):
         font_metrics = QFontMetrics(self.font())
-        text_width = font_metrics.width(self.text()) + 20
+        text_width = font_metrics.horizontalAdvance(self.text()) + 20
         self.setMinimumWidth(text_width)  # 设置最小宽度而不是固定宽度
 
     def enterEvent(self, event):
