408bcec48fd2f93059b65c43bc0dbab853b744ce
清安无别事 <18674622237@163.com>
2024-06-14 14:03:44 +0800

    修复串口未关闭的情况下再次打开奔溃

--- functions/serial_work.py
+++ functions/serial_work.py
@@ -13,6 +13,7 @@ class SerialWorker(QObject):
     def __init__(self, check_enter, check_hex_receive, check_hex_send):
         super().__init__()
         self.transport = None
+        self.protocol = None  # 保存 SerialProtocol 的引用
         self.check_enter = check_enter
         self.check_hex_receive = check_hex_receive
         self.check_hex_send = check_hex_send
@@ -25,7 +26,7 @@ class SerialWorker(QObject):
             await self.close_serial_port()
         try:
             loop = asyncio.get_event_loop()
-            self.transport, protocol = await serial_asyncio.create_serial_connection(
+            self.transport, self.protocol = await serial_asyncio.create_serial_connection(
                 loop, lambda: SerialProtocol(self, self.expected_length), port, baudrate)
             self.port_opened = True
             self.serial_connection_made.emit()
@@ -37,6 +38,7 @@ class SerialWorker(QObject):
             self.closing = True
             self.transport.close()
             self.transport = None
+            self.protocol = None  # 清空 protocol 引用
             self.port_opened = False
             await asyncio.sleep(0.1)  # 等待资源完全释放
             self.serial_connection_lost.emit()
@@ -45,10 +47,10 @@ class SerialWorker(QObject):
     def send_data(self, data):
         if self.transport:
             try:
-                if self.check_hex_send.toggled:
+                if self.check_hex_send.isChecked():
                     message = bytes.fromhex(data)
                 else:
-                    message = f"{data}\r\n".encode('utf-8') if self.check_enter.toggled else data.encode('utf-8')
+                    message = f"{data}\r\n".encode('utf-8') if self.check_enter.isChecked() else data.encode('utf-8')
                 self.transport.write(message)
                 self.data_sent.emit(data)
             except Exception as e:
@@ -59,8 +61,8 @@ class SerialWorker(QObject):
 
     def set_expected_length(self, length):
         self.expected_length = length
-        if self.transport and isinstance(self.transport.get_protocol(), SerialProtocol):
-            self.transport.get_protocol().set_expected_length(length)
+        if self.protocol:
+            self.protocol.set_expected_length(length)
 
 
 class SerialProtocol(asyncio.Protocol):
@@ -94,3 +96,6 @@ class SerialProtocol(asyncio.Protocol):
         if not self.worker.closing:
             self.worker.serial_connection_lost.emit()
         self.transport = None
+
+    def set_expected_length(self, length):
+        self.expected_length = length
